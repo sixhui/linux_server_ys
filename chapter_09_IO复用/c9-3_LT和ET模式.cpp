3f89633c91912a3abc86d10ad170190459879d32
liuxh <liuxh_cn@163.com>
2022-01-01 16:00:56 +0800

    epoll ET & LT - c9-3.cpp

--- chapter_09_IO复用/c9-3_LT和ET模式.cpp
+++ chapter_09_IO复用/c9-3_LT和ET模式.cpp
@@ -9,126 +9,143 @@
 #include <fcntl.h>
 using namespace std;
 
-#define MAX_EVENT_NUMBER 1024
-#define BUFFER_SIZE 10
+#define     MAX_EVENT_N         1024
+#define     MAX_LISTENED_SOCK_N 5
+#define     BUFFER_SIZE         10
+#define     oops(msg)           {perror(msg); exit(1);}
 
-/* 将文件描述符设置为非阻塞 */
-int setnonblocking(int fd);
-void addfd(int epollfd, int sfd, bool enable_et);
-void lt(epoll_event* events, int number, int epollfd, int listenfd);
-void et(epoll_event* events, int number, int epollfd, int listenfd);
+int     setnonblocking(int fd);
+void    addfd(int epollfd, int sfd, bool enable_et);
+void    lt(epoll_event* events, int number, int epollfd, int listenfd);
+void    et(epoll_event* events, int number, int epollfd, int listenfd);
 
 int main(int argc, char const *argv[])
 {
-    assert(argc == 1);
-
-    int port = 12345;
+    int ip      = INADDR_ANY;
+    int port    = 12345;
     int listenfd, backlog = 5;
-    int epollfd, sfd_num = 5;
-    int res;
-
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port);
+    int epollfd;
+    epoll_event events[MAX_EVENT_N];    // 接收 socket 就绪事件
+    int event_n;
 
     // socket
-    listenfd = socket(PF_INET, SOCK_STREAM, 0);
-    assert(listenfd >= 0);
+    if((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) oops("fail socket");
 
     // bind
-    res = bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
-    assert(res != -1);
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family        = AF_INET;
+    serv_addr.sin_addr.s_addr   = htonl(ip);
+    serv_addr.sin_port          = htons(port);
+
+    if((bind(listenfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr))) == -1) oops("fail bind");
 
     // listen
-    res = listen(listenfd, backlog);
-    assert(res != -1);
+    if((listen(listenfd, backlog)) == -1) oops("fail listen");
 
     // epoll
-    epoll_event events[MAX_EVENT_NUMBER];
-    epollfd = epoll_create(sfd_num);
-    assert(epollfd != -1);
-    addfd(epollfd, listenfd, true); /* 将监听 socket 添加到 epollfd，并启用 ET 模式*/
+    if((epollfd = epoll_create(MAX_LISTENED_SOCK_N)) == -1) oops("fail epoll_create");
+    addfd(epollfd, listenfd, true);     // 将 listenfd 添加到 epollfd，并启用 ET 模式
 
     while(1){
-        res = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
-        assert(res >= 0);
+        if((event_n = epoll_wait(epollfd, events, MAX_EVENT_N, -1)) < 0) oops("fail epoll_wait");
 
-        // lt(events, res, epollfd, listenfd);
-        et(events, res, epollfd, listenfd);
+        // lt(events, event_n, epollfd, listenfd);
+        et(events, event_n, epollfd, listenfd);
     }
     
     close(listenfd);
     return 0;
 }
 
+/* !!!只要 socket 读缓存还有未读出的数据就会触发 */
 void lt(epoll_event* events, int number, int epollfd, int listenfd){
-    char buf[BUFFER_SIZE];
+    char    buf[BUFFER_SIZE];
+    int     sockfd;
+    int     n_read;
+
     for(int i = 0; i < number; ++i){
-        int sockfd = events[i].data.fd;
-        if(sockfd == listenfd){ // 接收连接 socket，并添加到 epollfd 进行监听
-            struct sockaddr_in clnt_addr;
-            socklen_t clnt_addr_len = sizeof(clnt_addr);
-            int clnt_sock = accept(listenfd, (struct sockaddr*) &clnt_addr, &clnt_addr_len);
+        sockfd = events[i].data.fd;
+        if(sockfd == listenfd){             // listenfd 的事件：接受 clnt_sock，并添加到 epollfd 进行监听
+            int                 clnt_sock;
+            struct sockaddr_in  clnt_addr;
+            socklen_t           clnt_addr_len = sizeof(clnt_addr);
 
-            /* LT 方式监听客户端 socket */
-            addfd(epollfd, clnt_sock, false);
-        }
-        else if(events[i].events & EPOLLIN){ // 发生读就绪事件，接收数据
-            /* 只要 socket 读缓存还有未读出的数据就会触发 */
-            cout << "EPOLLIN LT trigger once" << endl;
-            memset(buf, '\0', BUFFER_SIZE);
-            int res = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
-
-            if(res <= 0) {
-                cout << "close" << endl;
-                close(sockfd); 
-                continue;
+            if((clnt_sock = accept(listenfd, 0, 0)) == -1) {
+                oops("fail accept == -1");
             }
-            cout << "GET " << res << " bytes of content: " << buf << endl;
+            else{
+                addfd(epollfd, clnt_sock, false);
+            }
         }
-        else cout << "something else happened" << endl;
+        else if(events[i].events & EPOLLIN){ // 读就绪事件：接收数据
+            cout << "EPOLLIN LT trigger once" << endl;
+
+            memset(buf, '\0', BUFFER_SIZE);
+            if((n_read = recv(sockfd, buf, BUFFER_SIZE - 1, 0)) <= 0){
+                cout << "close" << sockfd << endl;
+                close(sockfd);
+            }
+            else{
+                cout << "GET " << n_read << " bytes of content: " << buf << endl;
+            }
+        }
+        else{
+            cout << "something else happened" << endl;
+        }
     }
 }
 
+/* !!!不会重复触发，所以需要 while 确保数据全部读出 */
 void et(epoll_event* events, int number, int epollfd, int listenfd){
-    char buf[BUFFER_SIZE];
+    char    buf[BUFFER_SIZE];
+    int     sockfd;
+    int     n_read;
+    
     for(int i = 0; i < number; ++i){
-        int sockfd = events[i].data.fd;
+        sockfd = events[i].data.fd;
         if(sockfd == listenfd){
-            struct sockaddr_in clnt_addr;
-            socklen_t clnt_addr_len = sizeof(clnt_addr);
-            int clnt_sock = accept(sockfd, (struct sockaddr*) &clnt_addr, &clnt_addr_len);
-            
-            addfd(epollfd, clnt_sock, true);
+            int                 clnt_sock;
+            struct sockaddr_in  clnt_addr;
+            socklen_t           clnt_addr_len = sizeof(clnt_addr);
+
+            if((clnt_sock = accept(sockfd, 0, 0)) == -1) {
+                oops("fail accept == -1")
+            }
+            else{
+                addfd(epollfd, clnt_sock, true);
+            }
         }
         else if(events[i].events & EPOLLIN){
-            /* 不会重复触发，所以需要循环确保数据全部读出 */
             cout << "EPOLLIN ET trigger once" << endl;
+
             while(1){
                 memset(buf, '\0', BUFFER_SIZE);
-                int res = recv(sockfd, buf, BUFFER_SIZE - 1, 0);                    // 轮询非阻塞 fd
-                cout << "recv ->" << res << endl;
-                if(res == 0){                                                       // 对方关闭连接
-                    cout << "closed by client" << endl;                             
+                n_read = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
+                cout << "n read " << n_read << endl;
+                if(n_read > 0){
+                    cout << "READ ALREADY: " << buf << endl;               
+                }
+                else if(n_read == 0){
+                    cout << "CLOSE by CLNT" << endl;                             
                     close(sockfd);
                     break;
                 }
-                else if(res < 0 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))){   // 读就绪未发生 - 缓冲区低水位，自动重置 EPOLLIN-ET 事件
-                    cout << "read later" << endl;
+                else if(n_read < 0 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))){   // 数据读取完毕，自动重置 EPOLLIN-ET 事件
+                    cout << "READ LATER" << endl;
                     break;
                 }
-                else if(res < 0){                                                   // 出错
-                    cout << "close for error" << endl;
+                else if(n_read < 0){       
+                    cout << "CLOSE for ERROR" << endl;
                     break;
-                }
-                else {                                                              // 读就绪发生  - 收到数据
-                    cout << "get content：" << buf << endl;
                 }
             }
+
+            cout << "EPOLLIN ET exist" << endl;
         }
-        else cout << "something else happened" << endl;
+        else{
+            cout << "something else happened" << endl;
+        }
     }
 }
 
@@ -136,7 +153,7 @@ void et(epoll_event* events, int number, int epollfd, int listenfd){
 int setnonblocking(int fd){
     int old_option = fcntl(fd, F_GETFL);
     int new_option = old_option | O_NONBLOCK;
-    // fcntl(fd, F_SETFL, new_option);
+    fcntl(fd, F_SETFL, new_option);
     return old_option;
 }
 
